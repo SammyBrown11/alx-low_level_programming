e7bd87342f29bf3b107a9e1bfd919b8f1a8559f8
samuel <samuelpaulinus4@gmail.com>
2023-07-21 09:05:54 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,11 +4,13 @@
  * more_numbers - Print 10 times the numbers since 0 up to 14
  * Return: 10 times of the numbers since 0 up to 14
  */
+
 void more_numbers(void)
+
 {
     int x, y;
 
-    for (x = 0; x <= 9; x++) // Changed the condition to x <= 9
+    for (x = 0; x <= 9; x++)
     {
         for (y = 0; y <= 14; y++)
         {
