74e56e8beef9a7a1f7e112c83443757ca3675b19
samuel <samuelpaulinus4@gmail.com>
2023-09-20 07:08:06 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -13,7 +13,7 @@ void print_buffer(char *b, int size)
 
 	o = 0;
 
-	if (size <+ 0)
+	if (size <= 0)
 	{
 		printf("\n");
 		return;
@@ -21,4 +21,29 @@ void print_buffer(char *b, int size)
 	while (o < size)
 	{
 		j = size - o < 10 ? size - o : 10;
-		printf(
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+	}
+}
